8d3c31338903211b4d2b20a6f9bd7d132ee2e99b
Anthony Lukach <anthonylukach@gmail.com>
2026-05-07 12:00:20 -0600

    fix: correct base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  base: '/viewer-json-eo/',
+  base: '/json-viewer/',
   plugins: [
     react(),
     viteStaticCopy({
